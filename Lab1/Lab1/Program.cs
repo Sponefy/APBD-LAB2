f07568eab13e7c5ddb15f12e8c63d6b14d490a05
BialasPiotr <piotr@bialas.net.pl>
2024-03-12 13:32:40 +0100

    Use GetAverage

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -15,3 +15,7 @@ static double GetAverage(int[] numbers)
 
     return (double) sum / numbers.Length;
 }
+
+int[] numbers = {1, 2, 3, 4};
+double average = GetAverage(numbers);
+Console.WriteLine(average);
